c2e66b999b094f44f4f106956afa1dc97ef8b612
Robert Peralta <rperalta@sparknettech.com>
2016-10-07 10:36:36 -0400

    Update wkhtmltopdf install script

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -1,8 +1,31 @@
 set -e
 
+echo '============'
+echo install_base.sh
+echo '============'
 bash $METEORD_DIR/lib/install_base.sh
+
+echo '============'
+echo install_node.sh
+echo '============'
 bash $METEORD_DIR/lib/install_node.sh
+
+echo '============'
+echo install_phantomjs.sh
+echo '============'
 bash $METEORD_DIR/lib/install_phantomjs.sh
+
+echo '============'
+echo install_wkhtmltopdf.sh
+echo '============'
 bash $METEORD_DIR/lib/install_wkhtmltopdf.sh
+
+echo '============'
+echo install_imagemagick.sh
+echo '============'
 bash $METEORD_DIR/lib/install_imagemagick.sh
+
+echo '============'
+echo cleanup.sh
+echo '============'
 bash $METEORD_DIR/lib/cleanup.sh
--- base/scripts/lib/install_wkhtmltopdf.sh
+++ base/scripts/lib/install_wkhtmltopdf.sh
@@ -1,6 +1,4 @@
 #!/bin/bash#!/bin/bash
 set -e
 
-apt-get install -y build-essential xorg libssl-dev libxrender-dev wget gdebi
-wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
-gdebi --n wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+apt-get install -y wkhtmltopdf python-pip xvfb xauth xfonts-base
